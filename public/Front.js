f1a40d7032c20381787f9ef5d2ec2bc2e7247b57
AndreaLizeth Hdz Pérez <andrea.hernandezper@gmail.com>
2023-05-10 13:03:56 -0600

    HTML y CSS version inicial

--- public/Front.js
+++ public/Front.js
@@ -2,29 +2,36 @@ var barraBusqueda = document.querySelector('form');
 const input = document.querySelector('input');
 const button = document.getElementById('button2'); //Una etiqueta botón recarga la página, input type button no recarga la página, 
 
-button.addEventListener('click',function(){cambio()});
+button.addEventListener('click',function(){temperatura()}); //Evento click o submit***
 
 const h22 = document.getElementById("h2");
 const h2 = document.querySelector('h2');
 
-function cambio(){
+function temperatura(){
+    let ciudad = input.value;   //API se encuentra en inglés
     
-    h2.innerHTML = 'otro';
-    h2.innerText = 'otro';
-
-    
-    h22.innerHTML = "otro";
-    h22.innerText = "otro";
+    fetch('http://api.weatherstack.com/current?access_key=30a0c9b417883f7457f686d4c1abb198&query='+ciudad).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+        console.log(data.error)
+        } else {
+        console.log(data.location)
+        console.log(data.request)
+        console.log(data.current)
+        //console.log("Datos de interés: "+ data.location.name + data.location.country + data.request.query + data.current.temperature + data.current.weather_icons + data.current.wind_speed + data.current.humidity)
+        }
+        console.log("Datos de interés: "+ data.location.name + data.location.country + data.request.query + data.current.temperature + data.current.weather_icons + data.current.wind_speed + data.current.humidity)
+        
+        
+        h22.innerHTML = '<img src="'+data.current.weather_icons+'">';
+    })
+})
 
 }
 
-/*barraBusqueda.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const ciudad = input.value;*/
 
-//http://api.weatherstack.com/current?access_key=30a0c9b417883f7457f686d4c1abb198&query=New%20York
-var ciudad = '';    //API se encuentra en inglés
-
+/*
+let ciudad = 'New York';
 fetch('http://api.weatherstack.com/current?access_key=30a0c9b417883f7457f686d4c1abb198&query='+ciudad).then((response) => {
     response.json().then((data) => {
         if (data.error) {
@@ -36,13 +43,13 @@ fetch('http://api.weatherstack.com/current?access_key=30a0c9b417883f7457f686d4c1
         //console.log("Datos de interés: "+ data.location.name + data.location.country + data.request.query + data.current.temperature + data.current.weather_icons + data.current.wind_speed + data.current.humidity)
         }
         console.log("Datos de interés: "+ data.location.name + data.location.country + data.request.query + data.current.temperature + data.current.weather_icons + data.current.wind_speed + data.current.humidity)
-    
     })
-    
-})
-    
-    
+});*/
 
-    //const temperatura = 20;
 
-//})
+
+
+//http://api.weatherstack.com/current?access_key=30a0c9b417883f7457f686d4c1abb198&query=New%20York
+ 
+
+
