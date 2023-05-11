faadb48a5b663f358f262cae92e6f906ee8cff95
AndreaLizeth Hdz Pérez <andrea.hernandezper@gmail.com>
2023-05-10 18:26:32 -0600

    Mensajes de error agregados

--- public/Front.js
+++ public/Front.js
@@ -1,29 +1,69 @@
 var barraBusqueda = document.querySelector('form');
 const input = document.querySelector('input');
-const button = document.getElementById('button2'); //Una etiqueta botón recarga la página, input type button no recarga la página, 
+const button = document.getElementById('button'); //Una etiqueta botón recarga la página, input type button no recarga la página, 
 
-button.addEventListener('click',function(){temperatura()}); //Evento click o submit***
+const ciudadBuscada = document.getElementById('ciudadBuscada');
+const iconoClima = document.getElementById('iconoClima');
+const temperature = document.getElementById('temperature');
+const windSpeed = document.getElementById('windSpeed');
+const humidity = document.getElementById('humidity');
 
-const h22 = document.getElementById("h2");
-const h2 = document.querySelector('h2');
+const iconoClima2 = document.getElementById('iconoClima');
 
-function temperatura(){
+const contenedorGeneral = document.getElementById('contenedorGeneral');
+const templateError = document.getElementById('templateError');
+
+const form = document.querySelector('form');
+
+button.addEventListener('click',function(){datosClima()}); //Evento click o submit***
+
+function datosClima(){
     let ciudad = input.value;   //API se encuentra en inglés
     
     fetch('http://api.weatherstack.com/current?access_key=30a0c9b417883f7457f686d4c1abb198&query='+ciudad).then((response) => {
     response.json().then((data) => {
         if (data.error) {
         console.log(data.error)
+
+        if(document.getElementById('mensajeError') == null){
+            let templateClone = document.importNode(templateError.content, true);
+            contenedorGeneral.insertBefore(templateClone, form);
+        }
+        
+        let mensajeError = document.getElementById('mensajeError');
+            if(data.error.code == 105){
+                console.log('Lost connection with the server, try again later');
+                mensajeError.innerHTML = 'Lost connection with the server, try again later';
+            } else if(data.error.code == 601){
+                console.log('Input is empty');
+                mensajeError.innerHTML = 'Input is empty';
+            } else if(data.error.code == 615){
+                console.log('Request failed, try again');
+                mensajeError.innerHTML = 'Request failed, try again';
+            }
         } else {
         console.log(data.location)
         console.log(data.request)
         console.log(data.current)
-        //console.log("Datos de interés: "+ data.location.name + data.location.country + data.request.query + data.current.temperature + data.current.weather_icons + data.current.wind_speed + data.current.humidity)
-        }
         console.log("Datos de interés: "+ data.location.name + data.location.country + data.request.query + data.current.temperature + data.current.weather_icons + data.current.wind_speed + data.current.humidity)
         
+        if(document.getElementById('mensajeError')){
+            let mensajeError = document.getElementById('mensajeError');
+            contenedorGeneral.removeChild(mensajeError);
+        }
         
-        h22.innerHTML = '<img src="'+data.current.weather_icons+'">';
+
+        ciudadBuscada.innerHTML = data.request.query;
+        iconoClima.src = data.current.weather_icons; //'<img src="'+data.current.weather_icons+'">';
+        iconoClima.src = data.current.weather_icons;
+
+        temperature.innerHTML = data.current.temperature + ' °C';
+        windSpeed.innerHTML = data.current.wind_speed + ' km/h';
+        humidity.innerHTML = data.current.humidity + '%';
+
+        }
+        
+        
     })
 })
 
--- src/app.js
+++ src/app.js
@@ -19,44 +19,15 @@ app.get('', (req, res) => {
     })
     })
 
-/*
-var barraBusqueda = document.querySelector('form');
-const input = document.querySelector('input');
-barraBusqueda.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const ciudad = input.value;*/
-
-    //http://api.weatherstack.com/current?access_key=30a0c9b417883f7457f686d4c1abb198&query=New%20York
-let ciudad = 'New York';    //API se encuentra en inglés
-/*
-fetch('http://api.weatherstack.com/current?access_key=30a0c9b417883f7457f686d4c1abb198&query='+ciudad).then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-        console.log(data.error)
-        } else {
-        console.log(data.location)
-        console.log(data.request)
-        console.log(data.current)
-        //console.log("Datos de interés: "+ data.location.name + data.location.country + data.request.query + data.current.temperature + data.current.weather_icons + data.current.wind_speed + data.current.humidity)
-        }
-    })
-})*/
-
-    /*const temperatura = 20;
-
-})*/
-
-
-
+//http://api.weatherstack.com/current?access_key=30a0c9b417883f7457f686d4c1abb198&query=New%20York
 
 //Página no encontrada
-/*app.get('*', (req, res) => {
+app.get('*', (req, res) => {
     res.render('404', {
-    title: '404',
-    name: 'Andrew Mead',
-    errorMessage: 'Page not found.'
+        title: 'Error 404',
+        errorMessage: 'Page not found.'
     })
-    })*/
+})
 
 app.listen(3000, function() {
     console.log("App started on port 3000");
